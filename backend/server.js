b98355b615d005a3f12285783c583ea756b0076b
Udhaya0519 <udhaya0519@gmail.com>
2025-02-11 14:12:03 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -2,36 +2,41 @@ import express from "express"
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 import dotenv from "dotenv"
-import path from "path"
+import cors from 'cors'
 
 
 dotenv.config()
 
 
 const app = express();
-const __dirname = path.resolve()
+
+connectDB();
 
 app.use(express.json())
+
+app.use(
+    cors({
+      origin: [
+        "https://mern-auth-app-pi.vercel.app",
+        "https://mern-auth-app-udhaya-js-projects.vercel.app",
+      ],
+      credentials: true,
+    })
+  );
+
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://mern-auth-app-pi.vercel.app");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
 
 
 app.use("/api/products", productRoutes)
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")))
-    
-    app.get("*", (req,res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist"))
-    })
-}
 
-const PORT = 4000
 
-app.listen(PORT,() => {
 
-    connectDB();
-
-    console.log("server is running on port",PORT);
-    
-})
 
 
