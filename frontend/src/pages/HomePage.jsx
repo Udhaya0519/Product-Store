36e9e0dc1dc93fe0a0c61565923c773c3fac0d65
Udhaya0519 <udhaya0519@gmail.com>
2025-02-11 23:19:00 +0530

    updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { TbMoodCry } from "react-icons/tb";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 
 import { useProductStore } from "../store/product";
 import ProductCard from "../components/ProductCard";
@@ -64,21 +63,7 @@ function HomePage() {
                })}
             </div>
 
-            {isLoading ? (
-               <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
-                  <motion.div
-                     className="w-16 h-16 border-4 border-t-4 border-t-green-500 border-green-200 rounded-full"
-                     animate={{ rotate: 360 }}
-                     transition={{
-                        duration: 1,
-                        repeat: Infinity,
-                        ease: "linear",
-                     }}
-                  />
-               </div>
-            ) : (
-               <></>
-            )}
+            
 
             {products.length === 0 ? (
                <div className="product-not-found h-[50vh] flex flex-col sm:flex-row items-center justify-center">
