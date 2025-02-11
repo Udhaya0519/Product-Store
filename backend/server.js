e527c9782ae7b99ec1faaea66a0878519c7c0f25
Udhaya0519 <udhaya0519@gmail.com>
2025-02-11 14:23:39 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express"
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 import dotenv from "dotenv"
-import cors from 'cors'
+// import cors from 'cors'
 
 
 dotenv.config()
@@ -14,23 +14,6 @@ connectDB();
 
 app.use(express.json())
 
-app.use(
-    cors({
-      origin: [
-        "https://mern-auth-app-pi.vercel.app",
-        "https://mern-auth-app-udhaya-js-projects.vercel.app",
-      ],
-      credentials: true,
-    })
-  );
-
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://mern-auth-app-pi.vercel.app");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-  });
 
 
 app.use("/api/products", productRoutes)
